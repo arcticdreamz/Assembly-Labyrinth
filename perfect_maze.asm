e0a700a8738d78a2b5314413a4908e0d3f77bb69
Tom <tomcrasset@gmail.com>
2017-10-23 19:23:13 +0200

    Debuggage

--- perfect_maze.asm
+++ perfect_maze.asm
@@ -1,5 +1,4 @@
-.include beta.uasm
-.include main.asm
+
  |; ***************************************************************
 |; constants
 nb_rows = 8
@@ -12,13 +11,11 @@ nb_maze_words = 512
 cells_per_word = 4
 
 
-|; Reg[Rc] <- Reg[Ra] mod Reg[Rb] (Rc should be different from Ra and Rb)
-.macro MOD(Ra, Rb, Rc) DIV(Ra, Rb, Rc) MUL(Rc, Rb, Rc) SUB(Ra, Rc, Rc)
 
 .macro MODC(Ra, CC, Rc) DIVC(Ra, CC, Rc) MULC(Rc, CC, Rc) SUB(Ra, Rc, Rc)
 
 |; Swap the values stored at addresses a and b.
-.macro SWAP(Ra,Rb,Rc) MOVE(Rc,Ra) MOVE(Ra,Rb) MOVE(Rb,Rc)
+.macro SWAP(Ra,Rb,Rc) MOVE(Ra,Rc) MOVE(Rb,Ra) MOVE(Rc,Rb)
 
 |;Return the row of the cell at given index.
 .macro ROW_FROM_INDEX(Ra,Rb,Rc) DIV(Ra,Rb,Rc)
@@ -30,25 +27,77 @@ cells_per_word = 4
 
 .macro END() MOVE(BP,SP) POP(BP) POP(LP) RTN()
 
+valid_neighbour:
+	INIT()
+	PUSH(R9)
+	PUSH(R11)
+	PUSH(R7)
+	PUSH(R20)
+	PUSH(R10)
+	LD(BP, -28, R9)
+	LD(BP, -24, R11)
+	LD(BP, -20, R7)
+	LD(BP, -16, R20)
+	LD(BP, -12, R10)
 
+neigbour0:
+	CMPEQC(R9,0,R11)
+	BF(R11,neighbour4)
+	LD(R20,0,R10) 
+	ST(R7,0,R10)
+	BR(valid_neighbour_end)
+			
+neighbour4:
+	CMPEQC(R9,4,R11)
+	BF(R11,neighbour8)
+	LD(R20,4,R10) 
+	ST(R7,4,R10)  
+	BR(valid_neighbour_end)
+		
+neighbour8:
+	CMPEQC(R9,8,R11)
+	BF(R11,neighbour12)
+	LD(R20,8,R10) 
+	ST(R7,8,R10)  
+	BR(valid_neighbour_end)
 
+neighbour12:		
+	CMPEQC(R9,12,R11)
+	BF(R11,valid_neighbour_end)
+	LD(R20,12,R10) 
+	ST(R7,12,R10) 
+	BR(valid_neighbour_end)
 
+valid_neighbour_end:
+	
+	POP(R10)
+	POP(R20)
+	POP(R7)
+	POP(R11)
+	POP(R9)
+	POP(BP)
+	POP(LP)
+	RTN()
 
 
 
 |;allocate 4 words in memory for the neighbour array
-neighbours: ALLOCATE(4) 
+neighbours: 
+	STORAGE(4) 
 
 
 perfect_maze:
 	INIT()
 	CMOVE(neighbours,R20)
 
+
 	PUSH(R1) 
 	PUSH(R2)
 	PUSH(R3)
 	PUSH(R4)
 	PUSH(R6)
+	PUSH(R7)
+	PUSH(R10)
 
 	|; on loade les valeurs du stack dans les registres
 	LD(BP,-12,R1) |;maze --> R1
@@ -63,8 +112,7 @@ perfect_maze:
 	MODC(R6,32,R8) |; curr_cell in R6, (curr_cell % 32) 
 	SHL(R7,R8,R7) |; shift 1 (R7) left by R8 bits
 	DIVC(R6,8,R8) |; curr_cell/32 * 4
-	|;******* FAUT METTRE CC ET PAS UN REGISTRE SI CA MARCHE PAS
-	LD(R4,R8,R8) |; R8 = visited(R4)+offset(R8)
+	ADD(R8,R4,R8) |; R8 = visited(R4)+offset(R8)
 	OR(R8,R7,R8) |; update visited+offset
 
 init_neighbours:	
@@ -87,11 +135,18 @@ init_neighbours:
 	CMOVE(0,R9) |; n_valid_neighbours
 
 checkLeft: 
-	CMPLE(R31,R8,R7) |; 0 <= col(R8) ? Si faux, on l'ajoute
+	.breakpoint
+	CMPLEC(R8,0,R7) |; 0 <= col(R8) ? Si faux, on l'ajoute
 	BT(R7,checkRight)
 	SUBC(R6,1,R7) |; curr_cell - 1
-	LD(R20,R9,R10) 
-	ST(R7,R9,R10)  |;neighbours[n_valid_neighbours++] = curr_cell - 1;
+
+	PUSH(R9)
+	PUSH(R11)
+	PUSH(R7)
+	PUSH(R20)
+	PUSH(R10)
+	CALL(valid_neighbour)
+	DEALLOCATE(5)
 	ADDC(R9,4,R9) |;n_valid_neighbours++
 
 checkRight:
@@ -100,18 +155,31 @@ checkRight:
 	CMPLT(R8,R7,R7) |; col < nb_cols -1 (R7)
 	BF(R7,checkTop)
 	ADDC(R6,1,R7) |; curr_cell + 1
-	LD(R20,R9,R10)
-	ST(R7,R9,R10)
+
+	PUSH(R9)
+	PUSH(R11)
+	PUSH(R7)
+	PUSH(R20)
+	PUSH(R10)
+	CALL(valid_neighbour)
+	DEALLOCATE(5)
 	ADDC(R9,4,R9) |;n_valid_neighbours++
 
 	ROW_FROM_INDEX(R6,R5,R8) |; row in R8
 checkTop:
 	|;check top neighbour
-	CMPLE(R31,R8,R7) |; 0 <= row(R8) ? Si faux, on l'ajoute
+	CMPLEC(R8,0,R7) |; 0 <= row(R8) ? Si faux, on l'ajoute
 	BT(R7,checkBottom)
+
 	SUB(R6,R3,R7) |; curr_cell - nb_cols
-	LD(R20,R9,R10)
-	ST(R7,R9,R10)
+	PUSH(R9)
+	PUSH(R11)
+	PUSH(R7)
+	PUSH(R20)
+	PUSH(R10)
+	CALL(valid_neighbour)
+	DEALLOCATE(5)
+
 	ADDC(R9,4,R9) |;n_valid_neighbours++
 
 checkBottom:
@@ -120,8 +188,14 @@ checkBottom:
 	CMPLT(R8,R7,R7) |; row < nb_rows -1 (R7)
 	BF(R7,while_loop)
 	ADD(R6,R3,R7) |; curr_cell + nb_cols
-	LD(R20,R9,R10)
-	ST(R7,R9,R10)
+
+	PUSH(R9)
+	PUSH(R11)
+	PUSH(R7)
+	PUSH(R20)
+	PUSH(R10)
+	CALL(valid_neighbour)
+	DEALLOCATE(5)
 	ADDC(R9,4,R9) |;n_valid_neighbours++
 
 while_loop:
@@ -132,22 +206,30 @@ while_loop:
 	PUSH(R0)
 	CALL(abs__)
 	DEALLOCATE(1)
-
 	DIVC(R9,4,R7) |; n_valid_neighbours/4
-	MOD(R0,R7,R7) |; random_neigh_index (random % n_valid_neighbours/4)
-	MULC(R7,4,R7) |; random_neigh_index_offset (0 4 8 12)
-	LD(R20,R7,R19) |;neighbour = R8
-
+	MOD(R0,R7,R8) |; random_neigh_index (random % n_valid_neighbours/4)
+	MULC(R8,4,R8) |; random_neigh_index_offset (0 4 8 12)
+	ADD(R20,R7,R7) |; neighbour + random_neigh_index_offset
+	LD(R7,0,R19) |;neighbour = R19
+	.breakpoint
 	SUBC(R9,4,R11) |; n_valid_neighbours - 1 
-	LD(R20,R11,R11) |; neighbours + n_valid_neighbours - 1
-	LD(R20,R7,R12) |; neighbours + random_neigh_index
+
+
+	ADD(R20,R11,R11) |;R11 contient l'adresse de  neighbours + n_valid_neighbours - 1
+	LD(R11,0,R11) 
+	ADD(R20,R8,R7) |; neighbours + random_neigh_index
+	LD(R7,0,R12) 
+	.breakpoint
+
 	SWAP(R11,R12,R7)
 	SUBC(R9,4,R9) |; n_valid_neigbours--
 
 	MODC(R19,32,R7) |; neighbour % 32
 	DIVC(R19,8,R8) |; neighbour/32 * 4
-	LD(R4,R8,R8) |; visited[neighbour/32]
-	|; VERIFIER QU?ON EST BIEN EN LITTLB ENDIAN/BIG ENDIAN
+	ADD(R4,R8,R8)
+	LD(R8,0,R8) |; visited[neighbour/32]
+	.breakpoint
+
 	SHR(R8,R7,R8) |; Shift bitmap vers la droite de (neighbour % 32)
 	ANDC(R8,1,R8) |; visited_bit = R8
 	CMPEQC(R8,1,R7)
@@ -165,6 +247,8 @@ while_loop:
 	PUSH(R19) |; push neigbour (=! neighbours)
 	PUSH(R3) |; push nb_cols	
 	PUSH(R1) |; push maze
+	.breakpoint
+
 	CALL(connect__)			
 	DEALLOCATE(5)
 
@@ -173,6 +257,8 @@ while_loop:
 	PUSH(R3) |; cols
 	PUSH(R2) |; rows
 	PUSH(R1) |;maze
+	.breakpoint
+
 	CALL(perfect_maze)
 	DEALLOCATE(5)
 
@@ -213,6 +299,7 @@ connect__:
 	SWAP(R19,R6,R7)
 
 byte_offset:
+	.breakpoint
 	ROW_FROM_INDEX(R19,R3,R7)|; dest_row dans R7, R3 contient déjà col (cf main.asm)
 	MUL(words_per_row,R7,R11) |; row_offset dans R11
 	COL_FROM_INDEX(R6,R3,R7) |;source_col dans R7
@@ -254,14 +341,15 @@ vert_loop_init__:
 vert_loop__:
 	CMPEQC(R14,7, R7)
 	BT(R7,connect_end__)
-
+	.breakpoint
 	MULC(R14,words_per_mem_line,R7) |; iterator*words_per_mem_line
 	ADD(R11,R7,R7) |; word_offset + iterator*words_per_mem_line
 	
 	MULC(R7,4,R7) |; R7 now contains the adress of the *word* to be changed
-	LD(R1,R7,R15) |; load the word to R15
+	ADD(R1,R7,R7)
+	LD(R7,0,R15) |; load the word to R15
 	AND(R15,R13,R15) |; apply the mask
-	ST(R15,R7,R1) |; put the updated word back
+	ST(R15,0,R7) |; put the updated word back
 	ADDC(R14,1,R14) |; increment iterator
 	BR(vert_loop__)
 |;*************************************************************
@@ -292,23 +380,25 @@ horitonzal_loop_init__:
 horizontal_loop__:
 	CMPEQC(R14,2, R7)
 	BT(R7,connect_end__)
-	
+	.breakpoint
 	MULC(R14,words_per_mem_line,R7) |; iterator*words_per_mem_line
 	ADD(R11,R7,R7) |; word_offset + iterator*words_per_mem_line
 
 	MULC(R7,4,R7) |; R7 now contains the adress of the *word* to be changed
-	LD(R1,R7,R15) |; load the word to R15
+	ADD(R1,R7,R7)
+	LD(R7,0,R15) |; load the word to R15
 	AND(R15,R13,R15) |; apply the mask
-	ST(R15,R7,R1) |; put the updated word back
+	ST(R15,0,R7) |; put the updated word back
 	ADDC(R14,1,R14) |; increment iterator
 	BR(horizontal_loop__)
 
 connect_end__:
-POP(R1) |; POP maze
-POP(R3) |; POP nb_cols	
-POP(R19) |; POP neigbour
-POP(R4) |; POP visited
-POP(R6) |; POP curr_cell/source for connect
-END()
+	.breakpoint
+	POP(R1) |; POP maze
+	POP(R3) |; POP nb_cols	
+	POP(R19) |; POP neigbour
+	POP(R4) |; POP visited
+	POP(R6) |; POP curr_cell/source for connect
+	END()
 
 
